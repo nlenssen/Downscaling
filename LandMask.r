f70c5d24f6e3f10f6d56a0c19a0fac86142b4514
Nathan Lenssen <nlenssen@gmail.com>
2013-08-28 18:10:49 -0600

    Actually added landmask and production workflow files

---
+++ LandMask.r
@@ -0,0 +1,78 @@
+#A script used to create a land mask for prism wrt the parent grid
+
+ddir <- "/glade/p/work/lenssen/AR5"
+fname <- "tave_1895_2005.nc"
+prismvar <- "tave"
+
+library(fields)
+library(ncdf)
+
+prismnc    = open.ncdf(fname)
+prismlons  = get.var.ncdf(prismnc, "lon")
+prismlats  = get.var.ncdf(prismnc, "lat")
+topmost   = att.get.ncdf(prismnc,0,"North_Bounding_Coordinate")$value
+botmost   = att.get.ncdf(prismnc,0,"South_Bounding_Coordinate")$value
+lftmost   = att.get.ncdf(prismnc,0, "West_Bounding_Coordinate")$value
+rhtmost   = att.get.ncdf(prismnc,0, "East_Bounding_Coordinate")$value
+nlons     = length(prismlons)
+nlats     = length(prismlats)
+
+nlonsindarray = (1:nlons)
+nlatsindarray = (1:nlats)
+
+
+gcmfname = "TAS/tas_Amon_CCSM4_rcp26_r1i1p1_200601-210012.nc"
+gcmnc    = open.ncdf(paste(ddir,gcmfname,sep="/"))
+gcmlons  = get.var.ncdf(gcmnc, "lon")
+gcmlats  = get.var.ncdf(gcmnc, "lat")
+ngcmlons = length(gcmlons)
+ngcmlats = length(gcmlats)
+
+loninds = (1:ngcmlons)[(gcmlons >= lftmost) & (gcmlons <= rhtmost)]
+latinds = (1:ngcmlats)[(gcmlats >= botmost) & (gcmlats <= topmost)]
+ngcmlonconus = length(loninds)
+ngcmlatconus = length(latinds)
+gcmgrid = expand.grid(lon = gcmlons[loninds], lat = gcmlats[latinds])
+gcminds = expand.grid(lonind = loninds,             latind = latinds)
+gcmgrid = cbind(gcmgrid,gcminds)
+gcminds = expand.grid(gcmxi = (1:ngcmlonconus),gcmyj = (1:ngcmlatconus))
+gcmgrid = cbind(gcmgrid,gcminds)
+rm(gcminds)
+Ngridcells = nrow(gcmgrid)
+gcmdx      = mean(diff(gcmlons[loninds]))
+gcmdy      =      diff(gcmlats[latinds])
+
+# Get a prism Indicator
+tempPrism <- get.var.ncdf(prismnc, prismvar, c(1,1,1), c(-1,-1,1))
+prismLandIndicator <- !is.na(tempPrism)
+prismLocLand <- as.matrix(expand.grid(
+                                prismlons,prismlats))[prismLandIndicator,]
+
+parentmask<- matrix(NA,nrow=47,ncol=27)
+for(i in 1:Ngridcells){
+   targetlon = gcmgrid$lon[i]
+   targetlat = gcmgrid$lat[i]
+   gcmxi     = gcmgrid$gcmxi[i]
+   gcmyj     = gcmgrid$gcmyj[i]
+
+   x1 = targetlon - gcmdx/2.0
+   x2 = targetlon + gcmdx/2.0
+   y1 = targetlat - max(gcmdy)/2.0
+   y2 = targetlat + min(gcmdy)/2.0
+
+   loninds = nlonsindarray[(prismlons < x2) & (prismlons >= x1)]
+   latinds = nlatsindarray[(prismlats < y2) & (prismlats >= y1)]
+   pgrid   = expand.grid(loninds=loninds, latinds=latinds)
+
+   start   = c(      loninds[1],      latinds[1], 1 )
+   count   = c( length(loninds), length(latinds), 1 )
+   datmat  = get.var.ncdf(prismnc, prismvar, start, count)
+
+   if(all(is.na(datmat))){
+       val <- NA 
+   } else if(any(is.na(datmat))){
+       val <- FALSE
+   } else val <- TRUE
+
+   parentmask[gcmxi,gcmyj] <- val
+}
